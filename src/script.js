75b850f0c5bdfb9f8fbe1c568f1cfcb692045f30
Nathan Blaylock <ngblaylock@gmail.com>
2023-07-15 21:42:49 -0600

    Fix #14 for nested spans

--- src/script.js
+++ src/script.js
@@ -68,6 +68,17 @@ const insertFalsoBlock = (falsoObj) => {
     selection.addRange(newRange);
   }
 
+  // This fixes bug #14 where inserting a FalsoBlock inside another FalsoBlock will just replace the outer one with the new one.
+  let nestedFalsoBlock = document.querySelectorAll(
+    ".builder-content span[data-function] span[data-function]"
+  );
+  nestedFalsoBlock.forEach((span) => {
+    console.log(span.parentElement);
+    let parent = span.parentElement;
+    parent.after(span);
+    parent.remove();
+  });
+
   Alpine.store("data").showInsertTypes = false;
 };
 
